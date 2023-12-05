cb3f6c44bbd166172054f6e868ef831053487bb1
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-05 19:35:51 +0500

    Initial commit

---
+++ Kalkulyator.py
@@ -0,0 +1,11 @@
+def sum (a,b):
+  return a+b
+def deff (a,b):
+  return a-b
+def  mul (a,b):
+  return a*b
+def dic (a,b):
+  return (a/b)
+a=float (input ("a=")) b=float (input ("b=")) qu=sum (a,b) print
+("a+b=",qu) print ("a-b=",deff(a,b)) print ("a*b=",mul(a,b)) print
+("a/b=",dic(a,b))
