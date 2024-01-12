c94f9fab66cf824e0aefbb917e20c083be6a265c
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2024-01-12 21:11:59 +0500

    Create graphic.py

---
+++ graphic.py
@@ -0,0 +1,23 @@
+from turtle import *
+import colorsys
+speed(0)
+bgcolor("black")
+h=0.1
+pensize(4)
+def f():
+	global h
+	for i in range(4):
+		c=colorsys.hsv_to_rgb(h,1,1)
+		fillcolor(c)
+		h+=0.004
+		begin_fill()
+		fd(50)
+		rt(20)
+		fd(40)
+		rt(9)
+		end_fill()
+for j in range(300):
+	f()
+	goto(0,0)
+	rt(10)
+done()
