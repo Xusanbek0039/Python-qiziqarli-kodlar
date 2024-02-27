bb64cb86617f89308958fc410e76cf67d96f5338
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2024-02-27 20:34:37 +0500

    Create rasmni_ekranga_chiqarish.py

---
+++ rasmni_ekranga_chiqarish.py
@@ -0,0 +1,17 @@
+#tushuntirilgan videoni kurishilani sureman bulmasa tushunmaysilar, xurmat bilan Simcoder
+
+from tkinter import *
+from tkinter import PhotoImage
+
+oyna=Tk()
+oyna["bg"]="white"
+
+rasm=PhotoImage(file="rasm_joylashuvi")
+
+rasmni_chiqarish=Label(image=rasm)
+
+rasmni_chiqarish.pack()
+
+oyna.mainloop()
+
+
